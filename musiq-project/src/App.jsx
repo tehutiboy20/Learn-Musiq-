1e90c8df47639462245c48ba8976cb349226ea73
Amen Stoney <amenstoney@gmail.comgit config --global user.name Amen>
2025-12-03 23:46:23 -0500

    keyboard press working

--- musiq-project/src/App.jsx
+++ musiq-project/src/App.jsx
@@ -14,38 +14,61 @@ const noteData = [
 {note: "D4", frequency:293.66, key: 'w'},
 {note: "D#4", frequency:311.13, key: '3'},
 {note: "E4", frequency:329.63, key: 'e'},
-{note: "F4", frequency:349.23, key: '4'},
-{note: "F#4", frequency:369.99, key: 'r'}, 
-{note: "G4", frequency:392.00, key: '5'}, 
-{note: "G#4", frequency:415.30, key: 't'},
-{note: "A4", frequency:440.00, key: '6'},
-{note: "A#4", frequency:466.16, key: 'y'},
-{note: "B4", frequency:493.88, key: '7'},
-{note: "C5", frequency:523.25, key: 'u'},
-{note: "C#5", frequency:554.37, key: 'i'},
-{note: "D5", frequency:587.33, key: '9'},
-{note: "D#5", frequency:622.25, key: 'o'},
-{note: "E5", frequency:659.25, key: '0'},
-{note: "F5", frequency:698.46, key: 'p'},
-{note: "F#5", frequency:739.99, key: 'z'},
-{note: "G5", frequency:783.99, key: 's'},
-{note: "G#5", frequency:830.61, key: 'x'},
-{note: "A5", frequency:880.00, key: 'd'},
-{note: "A#5", frequency:932.33, key: 'c'},
-{note: "B5", frequency:987.77, key: 'f'},
-{note: "C6", frequency:1046.50, key: 'v'},
-{note: "C#6", frequency:1108.73, key: 'g'},
-{note: "D6", frequency:1174.66, key: 'b'},
-{note: "D#6", frequency:1244.51, key: 'h'},
-{note: "E6", frequency:1318.51, key: 'n'},
-{note: "F6", frequency:1396.91, key: 'j'},
-{note: "F#6", frequency:1479.98, key: 'm'},
-{note: "G6", frequency:1567.98, key: 'k'},
+{note: "F4", frequency:349.23, key: 'r'},
+{note: "F#4", frequency:369.99, key: '5'}, 
+{note: "G4", frequency:392.00, key: 't'}, 
+{note: "G#4", frequency:415.30, key: '6'},
+{note: "A4", frequency:440.00, key: 'y'},
+{note: "A#4", frequency:466.16, key: '7'},
+{note: "B4", frequency:493.88, key: 'u'},
+{note: "C5", frequency:523.25, key: 'i'},
+{note: "C#5", frequency:554.37, key: '9'},
+{note: "D5", frequency:587.33, key: 'o'},
+{note: "D#5", frequency:622.25, key: '0'},
+{note: "E5", frequency:659.25, key: 'p'},
+{note: "F5", frequency:698.46, key: 'z'},
+{note: "F#5", frequency:739.99, key: 's'},
+{note: "G5", frequency:783.99, key: 'x'},
+{note: "G#5", frequency:830.61, key: 'd'},
+{note: "A5", frequency:880.00, key: 'c'},
+{note: "A#5", frequency:932.33, key: 'f'},
+{note: "B5", frequency:987.77, key: 'v'},
+{note: "C6", frequency:1046.50, key: 'b'},
+{note: "C#6", frequency:1108.73, key: 'h'},
+{note: "D6", frequency:1174.66, key: 'n'},
+{note: "D#6", frequency:1244.51, key: 'j'},
+{note: "E6", frequency:1318.51, key: 'm'},
+{note: "F6", frequency:1396.91, key: ','},
+{note: "F#6", frequency:1479.98, key: 'l'},
+{note: "G6", frequency:1567.98, key: '.'},
 ]
 
 function handleKeyPress(event) {
-  
+
+  //get the presssed key 
+const pressedKey = event.key;
+
+// find matching note
+const matchedNote = noteData.find((note) => {
+// check if the key in the note data matches the pressed key
+return note.key === pressedKey
+
+}) 
+
+if(matchedNote) {
+  // calling handle play tone  
+  handlePlayTone(matchedNote.frequency, 0.5);
 }
+
+}
+useEffect(() => {
+  window.addEventListener('keydown', handleKeyPress);
+  
+  return () => {
+    window.removeEventListener('keydown', handleKeyPress);
+  };
+}, []);
+
 
 
   // instrument tone generation 
